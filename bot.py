995ed0b20030b1fbf690991686ba0c3acd80ab8b
Nikola Goergiev <48789303+nikogeorgiev@users.noreply.github.com>
2019-06-17 10:46:22 +0300

    Update bot.py
    A lot of changes

--- bot.py
+++ bot.py
@@ -1,70 +1,56 @@
 import pywikibot
 import pandas as pd
+import time
+import os
+reference = "~/Desktop/AEref.csv"
+site = pywikibot.Site("wikidata", "wikidata")
+repo = site.data_repository()
 
 
 def ver_claims(reffile, property, target):
 
     refdata = pd.read_csv(reffile, index_col=False)
-    site = pywikibot.Site("wikidata", "wikidata")
-    repo = site.data_repository()
 
     for index, item in refdata["PersID"].iteritems():
-
         itemwd = pywikibot.ItemPage(repo, item)
-        itemwd = itemwd.get()
-        label = itemwd
-        itemaff = pywikibot.ItemPage(repo, refdata["SecID"][index])
-        itemaff = itemaff.get()
-        item_dict = itemwd["claims"]
-        item_prop = item_dict[property]
-
-
+        itemcheck = itemwd
+        if itemcheck.isRedirectPage():
+            itemcheck = itemcheck.getRedirectTarget()
+            itemcheck = itemcheck.getID()
+            itemwd = pywikibot.ItemPage(repo, itemcheck)
+            itemcheck = itemwd
+            itemwd = itemwd.get()
+            item_dict = itemwd['claims']
+            item_prop = item_dict[property]
+            print(itemcheck, ' IS a redirect, changing to -> ', itemwd['labels']['en'])
+        else:
+            itemwd = itemwd.get()
+            item_dict = itemwd['claims']
+            try:
+                item_prop = item_dict[property]
+            except KeyError:
+                memof = pywikibot.page.Claim(repo, 'P463', is_qualifier=False)
+                memof.setTarget(pywikibot.ItemPage(repo, target))
+                itemcheck.addClaim(memof)
+                time.sleep(5)
+                itemwd = itemcheck
+                itemwd = itemwd.get()
+                item_dict = itemwd['claims']
+                item_prop = item_dict[property]
         for claim in item_prop:
-
             claim_trgt = claim.getTarget()
-
             if claim_trgt.getID() == target:
-                if claim.has_qualifier("P580", refdata["Year"][index]):
-                    try:
-                        print(itemwd["labels"]["en"], " HAS Start Time - ", refdata["Year"][index])
-                    except KeyError:
-                        print(item, " DOES NOT HAVE EN LABEL, ADDING LABEL ", refdata["name"][index])
-                        print(item, " HAS Start Time - ", refdata["Year"][index]
-                else:
-                    try:
-                        print(itemwd["labels"]["en"], " DOES NOT HAVE Start Time - ", refdata["Year"][index])
-                    except KeyError:
-                        print(item, " DOES NOT HAVE EN LABEL")
-                        print(item, " DOES NOT HAVE Start Time - ", refdata["Year"][index])
-                    finally:
-                        print("ADDING QUALIFIER P580 WITH VALUE ",  refdata["Year"][index], " TO ", item)
-                        start_date = pywikibot.page.Claim(repo, 'P580', is_qualifier=True)
-                        start_date.setTarget(pywikibot.WbTime(year=refdata["Year"][index]))
-                        claim.addQualifier(start_date,  summary = 'Adding start date')
-                if claim.has_qualifier("P1416", refdata["SecID"][index]):
-                    try:
-                        print(itemwd["labels"]["en"], " HAS Affiliation - ", itemaff["labels"]["en"])
-                    except KeyError:
-                        print(item, " DOES NOT HAVE EN LABEL")
-                        print(item, " HAS Affiliation - ", itemaff["labels"]["en"])
-                else:
-                    try:
-                        print(itemwd["labels"]["en"], " DOES NOT HAVE Affiliation - ", itemaff["labels"]["en"])
-                    except KeyError:
-                        print(item, " DOES NOT HAVE EN LABEL")
-                        print(item, " DOES NOT HAVE Affiliation - ", itemaff["labels"]["en"])
-                    finally:
-                        print("ADDING QUALIFIER P1416 WITH VALUE ", itemaff["labels"]["en"], " TO ", item)
-                        affiliation = pywikibot.page.Claim(repo, "P1416", is_qualifier= True)
-                        affiliation.setTarget(pywikibot.ItemPage(repo, refdata["SecID"][index]))
-                        claim.addQualifier(affiliation, summary = "Adding affiliation")
+                try:
+                    itemwd['labels']['en']
+                except KeyError:
+                    itemcheck.editLabels({'en': refdata['name'][index]})
+                if not claim.has_qualifier("P580", refdata["Year"][index]):
+                    start_date = pywikibot.page.Claim(repo, 'P580', is_qualifier=True)
+                    start_date.setTarget(pywikibot.WbTime(year=refdata["Year"][index]))
+                    claim.addQualifier(start_date)
+                if not claim.has_qualifier("P1416", refdata["SecID"][index]):
+                    affiliation = pywikibot.page.Claim(repo, "P1416", is_qualifier=True)
+                    affiliation.setTarget(pywikibot.ItemPage(repo, refdata["SecID"][index]))
+                    claim.addQualifier(affiliation)
 
-reffile = "~/Desktop/AEref.csv"
-
-ver_claims(reffile,"P463","Q337234")
-
-
-
-
-
-
+ver_claims(reference, "P463", "Q337234")
